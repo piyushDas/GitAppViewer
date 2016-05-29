4f16613942b5061cefa836aad4934f6d579b03c3
prakashsaripaka <prakashsaripaka@gmail.com>
2016-05-29 16:35:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-
-
 (function() {
 
-    var UserController = function ($scope,$http) { 
-       
-    $scope.submitTask = function() {
-        console.log('submitting task', $scope.newuser);
-        //var user=$scope.newuser;
-        //var str="https://api.github.com/users/"+user+"/repos";
-    $http.get("https://api.github.com/users/"+$scope.newuser+"/repos")
-   .success(function (response) {$scope.data = response;});
-           }
-        
+    var UserController = function ($scope,$http) {       
+            /*$http.get("https://api.github.com/users/tomtt").success(
+                function(response) 
+                {
+                    $scope.person = response;
+                    console.log(response)
+                }
+                );*/
+
+        $scope.fun=function()
+            {
+                    //console.log("hi");
+                    $http.get("https://api.github.com/search/repositories?q="+$scope.fname).success(
+                function(response) 
+                {
+                    $scope.person = response;
+                    console.log(response);
+                }
+                );
+            }
+
         $scope.showMessage = function () {
             $scope.message = "Hello, World!";
         };
@@ -20,9 +29,17 @@
             $scope.alert = "Saved...";
         };
 
-        /*$scope.person = person;*/
+        //$scope.person = person;
+        $scope.sort = function(keyname){
+        $scope.sortKey = keyname;   //set the sortKey to the param passed
+        $scope.reverse = !$scope.reverse; //if true make it false and vice versa
+        };
+
     };
 
-    var app = angular.module("githubViewer", []);
+    
+
+
+    var app = angular.module("githubViewer", ['angularUtils.directives.dirPagination']);
     app.controller("UserController", UserController);
 }());
