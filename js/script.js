43d30b547f31de33100049514e60eb07aa805137
nitish173 <emailnitish22@yahoo.in>
2016-08-27 13:08:44 +0530

    folder restructure and code refractor

---
+++ js/script.js
@@ -0,0 +1,30 @@
+(function() {
+
+    var UserController = function($scope, $http) {
+        $scope.fun = function() {
+            $http.get("https://api.github.com/search/repositories?q=" + $scope.fname).success(
+                function(response) {
+                    $scope.person = response;
+                    console.log(response);
+                }
+            );
+        }
+
+        $scope.showMessage = function() {
+            $scope.message = "Hello, World!";
+        };
+
+        $scope.saveMessage = function() {
+            $scope.alert = "Saved...";
+        };
+
+        //$scope.person = person;
+        $scope.sort = function(keyname) {
+            $scope.sortKey = keyname; // set the sortKey to the param passed
+            $scope.reverse = !$scope.reverse; // if true make it false and vice versa
+        };
+
+    };
+    var app = angular.module("githubViewer", ['angularUtils.directives.dirPagination']);
+    app.controller("UserController", UserController);
+}());
